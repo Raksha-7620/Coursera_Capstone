ef8999d1167bfd9288e34cd9306929e4cf59239a
Raksha-7620 <66378016+Raksha-7620@users.noreply.github.com>
2020-06-10 17:57:23 +0530

    Add files via upload

---
+++ data science.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This is notebook generated for data science project
+
+# ### A data science capstone project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import Numpy as np
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
